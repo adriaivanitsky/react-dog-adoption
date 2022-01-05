e8564d98675504e2b1ea234509fc2261214b5e01
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-04 18:15:25 -0800

    tryna deploy on netlify

--- src/components/DogDetail.js
+++ src/components/DogDetail.js
@@ -1,9 +1,6 @@
-import { Link } from 'react-router-dom';
-
 export default function DogDetail({ name, image, breed, bio }) {
   return (
     <>
-      <h1>individual dog</h1>
       <h1>{name}</h1>
       <div>
         <p>
