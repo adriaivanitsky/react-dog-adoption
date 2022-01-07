1f14e49933ec547fe8cac13c7e817b85d47f1962
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-07 15:36:59 -0800

    connected app to my own database and set up routes and imports to new admin and edit dog pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import Dogs from './views/Dogs';
 import DogInfo from './views/DogInfo';
+import Admin from './views/Admin';
+import EditDog from './views/EditDog';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <p>find your fuzzy companion today!</p> */}
           <Route exact path="/" component={Dogs} />;{/* <Route exact path="/dogs" />; */}
           <Route exact path="/dogs/:id" component={DogInfo} />;
+          <Route exact path="/admin" component={Admin} />;
+          <Route exact path="/dogs/:id/edit" component={EditDog} />
         </Switch>
       </div>
     </BrowserRouter>
