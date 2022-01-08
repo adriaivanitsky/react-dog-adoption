3df355eeef08afd30052a49c6b4be0b1f6d8a9ac
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-07 16:28:40 -0800

    created state and passed them into presentational components as props

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,58 @@
 import React from 'react';
 import './form.css';
 
-export default function Form() {
+export default function Form({
+  name,
+  age,
+  bio,
+  image,
+  breed,
+  setName,
+  setAge,
+  setBio,
+  setImage,
+  setBreed,
+}) {
   return (
     <form>
       <label>
-        name<input type="text" placeholder="name here"></input>
+        name
+        <input
+          type="text"
+          placeholder="name here"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
       </label>
       <label>
-        age<input type="number" placeholder="age"></input>
+        age
+        <input
+          type="number"
+          placeholder="age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        ></input>
       </label>
       <label>
-        bio<input type="text" placeholder="bio"></input>
+        bio
+        <input
+          type="text"
+          placeholder="bio"
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+        ></input>
       </label>
       <label>
-        image<input type="text"></input>
+        image<input type="text" value={image} onChange={(e) => setImage(e.target.value)}></input>
       </label>
       <label>
-        breed<input type="text" placeholder="breed"></input>
+        breed
+        <input
+          type="text"
+          placeholder="breed"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        ></input>
       </label>
       <button>save</button>
     </form>
