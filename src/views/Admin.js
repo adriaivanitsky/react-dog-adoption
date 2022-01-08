a45d4d168eda2b4d06d9420f0f38d22ac9101432
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-07 16:32:05 -0800

    created state and passed as props from admin to form

--- src/views/Admin.js
+++ src/views/Admin.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Header from '../components/Header';
 import Form from '../components/Form';
+import { useState } from 'react';
 
 export default function Admin() {
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [bio, setBio] = useState('');
+  const [image, setImage] = useState('');
+  const [breed, setBreed] = useState('');
+
   return (
     <>
       <Header />
-      <Form />
+      <Form
+        {...{
+          loading,
+          setLoading,
+          name,
+          setName,
+          age,
+          setAge,
+          bio,
+          setBio,
+          image,
+          setImage,
+          breed,
+          setBreed,
+        }}
+      />
     </>
   );
 }
